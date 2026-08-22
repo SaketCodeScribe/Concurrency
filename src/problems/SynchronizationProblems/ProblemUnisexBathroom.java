6da8b80c1123b1500f1cb4b07bee37074ff446c6
SaketCodeScribe <saket0565@gmail.com>
2026-08-23 01:14:46 +0530

    revision

--- src/problems/SynchronizationProblems/ProblemUnisexBathroom.java
+++ src/problems/SynchronizationProblems/ProblemUnisexBathroom.java
@@ -5,10 +5,14 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class ProblemUnisexBathroom {
+    enum Gender {
+        Male,
+        Female;
+    }
     private final Lock lock = new ReentrantLock(true);
     private final Condition canMaleEnter = lock.newCondition();
     private final Condition canFemaleEnter = lock.newCondition();
-    private char switchGender = 0;
+    private Gender switchGender = null;
     private final int capacity;
     private int maleCnt, femaleCnt, maleWaiting, femaleWaiting;
 
@@ -16,37 +20,37 @@ public class ProblemUnisexBathroom {
         this.capacity = capacity;
     }
 
-    public void useWashRoom(char gender) throws InterruptedException {
+    public void useWashRoom(Gender gender) throws InterruptedException {
         enter(gender);
         try {
-            System.out.println(gender + " using bathroom");
+            System.out.println(gender.toString() + " using bathroom");
             Thread.sleep(1000);
         } finally {
             leave(gender);
         }
     }
 
-    private void enter(char gender) throws InterruptedException {
+    private void enter(Gender gender) throws InterruptedException {
         lock.lock();
         try {
-            if (gender == 'M') {
+            if (gender == Gender.Male) {
                 maleWaiting++;
-                while (femaleCnt > 0 || maleCnt == this.capacity || switchGender == 'F') {
+                while (femaleCnt > 0 || maleCnt == this.capacity || switchGender == Gender.Female) {
                     canMaleEnter.await();
                 }
                 maleWaiting--;
                 maleCnt++;
             } else {
                 femaleWaiting++;
-                while (maleCnt > 0 || femaleCnt == this.capacity || switchGender == 'M') {
+                while (maleCnt > 0 || femaleCnt == this.capacity || switchGender == Gender.Male) {
                     canFemaleEnter.await();
                 }
                 femaleWaiting--;
                 femaleCnt++;
             }
         } catch (InterruptedException e) {
-            if (gender == 'M') maleWaiting--;
-            if (gender == 'F') femaleWaiting--;
+            if (gender == Gender.Male) maleWaiting--;
+            if (gender == Gender.Female) femaleWaiting--;
             Thread.currentThread().interrupt();
             throw e;
         } finally {
@@ -54,23 +58,23 @@ public class ProblemUnisexBathroom {
         }
     }
 
-    private void leave(char gender) {
+    private void leave(Gender gender) {
         lock.lock();
         try {
-            if (gender == 'M') maleCnt--;
+            if (gender == Gender.Male) maleCnt--;
             else femaleCnt--;
             if (maleCnt == 0 && femaleCnt == 0) {
-                if (gender == 'F' && maleWaiting > 0) {
+                if (gender == Gender.Female && maleWaiting > 0) {
                     canMaleEnter.signalAll();
-                    switchGender = 'M';
-                } else if (gender == 'M' && femaleWaiting > 0) {
+                    switchGender = Gender.Male;
+                } else if (gender == Gender.Male && femaleWaiting > 0) {
                     canFemaleEnter.signalAll();
-                    switchGender = 'F';
-                } else if (gender == 'M' && maleWaiting > 0) canMaleEnter.signalAll();
-                else if (gender == 'F' && femaleWaiting > 0) canFemaleEnter.signalAll();
-                else switchGender = 0;
-            } else if (gender == 'M' && femaleWaiting == 0 && maleWaiting > 0 && maleCnt < this.capacity) canMaleEnter.signalAll();
-            else if (gender == 'F' && maleWaiting == 0 && femaleWaiting > 0 && femaleCnt < this.capacity) canFemaleEnter.signalAll();
+                    switchGender = Gender.Female;
+                } else if (gender == Gender.Male && maleWaiting > 0) canMaleEnter.signalAll();
+                else if (femaleWaiting > 0) canFemaleEnter.signalAll();
+                else switchGender = null;
+            } else if (gender == Gender.Male && femaleWaiting == 0 && maleWaiting > 0 && maleCnt < this.capacity) canMaleEnter.signalAll();
+            else if (gender == Gender.Female && maleWaiting == 0 && femaleWaiting > 0 && femaleCnt < this.capacity) canFemaleEnter.signalAll();
         } finally {
             lock.unlock();
         }
